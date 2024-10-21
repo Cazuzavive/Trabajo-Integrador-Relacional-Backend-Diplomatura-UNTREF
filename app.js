14bcb0fa273634f9e5ae8d8ea60b7b63042267cd
cazuzavive <nefertaritheunique@gmail.com>
2024-10-21 16:21:19 -0300

    conexion a mysql

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const contenidoRoutes = require('./routes/contenidoRoutes');
 const db = require('./conexion/database');
+require('dotenv').config();
+console.log(process.env.PRUEBA);
 
 // Middlewares
 app.use(express.json());
@@ -12,4 +14,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-    
